8a6af443c9b07bb84cec7d3ba4277a10f7959173
kervinpierre <kervin@citymsp.nyc>
2017-11-20 20:17:06 -0500

    Application loads

--- src/main/java/nyc/citymsp/tools/azlogtool/main/AzlogtoolApplication.java
+++ src/main/java/nyc/citymsp/tools/azlogtool/main/AzlogtoolApplication.java
@@ -2,11 +2,24 @@ package nyc.citymsp.tools.azlogtool.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-public class AzlogtoolApplication {
+@SpringBootApplication(scanBasePackages="nyc.citymsp.tools.azlogtool")
+@EnableJpaRepositories("nyc.citymsp.tools.azlogtool.repository")
+@EntityScan(basePackages = "nyc.citymsp.tools.azlogtool.entity")
+public class AzlogtoolApplication extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(AzlogtoolApplication.class);
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 		SpringApplication.run(AzlogtoolApplication.class, args);
-	}
+    }
 }
